8eebf309019f0924077c2644a76934b371520485
atulb123 <30617666+atulb123@users.noreply.github.com>
2019-10-23 15:21:32 +0530

    Add files via upload

--- tests/Test.js
+++ tests/Test.js
@@ -1,20 +1,2 @@
-var Excel = require('exceljs');
-function test() {
-    
-var workbook = new Excel.Workbook();
-workbook.xlsx.readFile("C:/Users/acer/Desktop/Book1.xlsx")
-    .then(function() {
-        let rowcount=workbook.getWorksheet("Sheet1").rowCount;
-        let colcount=workbook.getWorksheet("Sheet1").columnCount;
-        for(let i=1;i<=rowcount;i++)
-        {
-            for(let j=1;j<=colcount;j++)
-            {
-                if(workbook.getWorksheet("Sheet1").getRow(i).getCell(j).value!=null)
-                console.log(workbook.getWorksheet("Sheet1").getRow(i).getCell(j).value);
-            }
-        }
-    });
-    
-}
-test();
+let Sample=require('../Utils/Sample')
+Sample.excelInteraction()
